cbd0ae1b563c73ee41e8962d9db0b90bd6723832
DanielKaminsky05 <daniel.kaminsky23@gmail.com>
2025-11-14 14:11:55 -0500

    added supabase ssr

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,10 @@
 "use client"
-import Link from "next/link"
-import { useRouter } from "next/navigation"
 export default function Login(){
 
   
     return (
         <div className = 'flex justify-center items-center '>
-            <p >Login Page</p>
+            <p>Login</p>
             
         </div>
     )
---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+export default function Signup(){
+
+  
+    return (
+        <div className = 'flex justify-center items-center '>
+            <p>Signup page</p>
+            
+        </div>
+    )
+}
