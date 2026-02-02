a30eb790d0ad00b16c0f6d5028f30db7a6b283f1
DanielKaminsky05 <daniel.kaminsky23@gmail.com>
2026-02-02 11:49:14 -0500

    added redirection of authenticated users away from login to avoid confusion

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -42,6 +42,12 @@ export async function updateSession(request: NextRequest) {
 
   const user = claims
 
+  if (user && request.nextUrl.pathname.startsWith('/login')) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/profiles'
+    return NextResponse.redirect(url)
+  }
+
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
