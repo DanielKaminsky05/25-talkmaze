9105479f6cfe39a3db36de538ae312d60c30c75b
Tristan Li <tristansup5@gmail.com>
2026-03-13 00:26:01 -0400

    feat: Added subscription management button

--- app/(protected)/components/NavigationBar.tsx
+++ app/(protected)/components/NavigationBar.tsx
@@ -3,9 +3,18 @@ import Image from "next/image";
 import StartVideoLessonBox from "./StartVideoLessonBox";
 import AvatorIcon from "./AvatorIcon";
 import { signOut } from "@/lib/auth/signout";
+import { cookies } from "next/headers";
 
 //Navigation Bar Component
-export default function NavigationBar() {
+export default async function NavigationBar() {
+
+  const cookieStore = await cookies()
+  
+  const role = cookieStore.get('active_profile_type')?.value
+
+  if(role == undefined){
+    throw new Error("Unable to validate user profile type")
+  }
   const router = useRouter();
   const pathname = usePathname();
 
@@ -45,6 +54,14 @@ export default function NavigationBar() {
         >
           Sign Out
         </button>
+
+         {
+          role == 'parent' && (
+            <button className = 'w-30 h-10'>
+              Manage Subscriptions
+            </button>
+          )
+         }
         <StartVideoLessonBox />
         <AvatorIcon />
       </div>
