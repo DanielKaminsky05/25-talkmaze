ec1fd99a915f346e3809f15611fac2a0c1560e69
Benson Li <bensonlangli@gmail.com>
2026-04-24 12:43:41 -0400

    refactor: Move student home specific components under /home

--- app/(protected)/home/_components/UpNextLesson.tsx
+++ app/(protected)/home/_components/UpNextLesson.tsx
@@ -16,7 +16,9 @@ export default function NextLessonCard({
       onClick={onClick}
       className="relative rounded-xl shadow-[0px_4px_4px_rgba(0,0,0,0.25)] overflow-hidden cursor-pointer w-full h-[244px]"
       style={{
-        background: "linear-gradient(90deg, #B1E7D6 0%, #B1E7D6 100%)",
+        backgroundImage: "url('/lesson-card-bg.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       {/* Label pill */}
