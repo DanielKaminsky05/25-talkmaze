51b27d5f1d4d879cf081bea967ea1b8937335bbf
Benson Li <bensonlangli@gmail.com>
2026-05-05 00:15:12 -0400

    refactor: Colocate Calendar component to /parent/sessions

--- app/(protected)/parent/sessions/_components/ParentSessionsClient.tsx
+++ app/(protected)/parent/sessions/_components/ParentSessionsClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMemo, useState } from "react";
-import Calendar from "../../../_components/calendar/Calendar";
+import Calendar from "./Calendar";
 import { CalendarDays, ChevronDown, User } from "lucide-react";
 import AvailabilityModal from "./AvailabilityModal";
 
