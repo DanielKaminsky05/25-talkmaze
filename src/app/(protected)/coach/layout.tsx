caae948853e72e49f0db7cc2690c28a605c69bfc
Benson Li <bensonlangli@gmail.com>
2026-05-12 14:36:39 -0400

    feat(coach): Modify coach navbar to have better navigation UI

--- src/app/(protected)/coach/layout.tsx
+++ src/app/(protected)/coach/layout.tsx
@@ -1,9 +1,10 @@
-
 import { ReactNode } from "react";
+import CoachNavbar from "./_components/CoachNavbar";
 
 export default function coachLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-[#2B4257]">
+    <div className="min-h-screen bg-[#2B4257] flex flex-col relative">
+      <CoachNavbar />
       {children}
     </div>
   );
